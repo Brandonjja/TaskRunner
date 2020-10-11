a1e60127675cf9c5baebf62d8f38be468d348d36
Brandon <brandonjja@hotmail.com>
2020-10-11 07:01:30 -0300

    Blaze rods/obsidian will no longer drop on death

--- src/com/brandonjja/taskRun/listeners/player/PlayerDeathListener.java
+++ src/com/brandonjja/taskRun/listeners/player/PlayerDeathListener.java
@@ -1,12 +1,27 @@
 package com.brandonjja.taskRun.listeners.player;
 
+import java.util.Iterator;
+
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerDeathListener implements Listener {
+	
+	@EventHandler
+	public void onDeath(PlayerDeathEvent e) {
+		Iterator<ItemStack> it = e.getDrops().iterator();
+		
+		while (it.hasNext()) {
+			ItemStack stack = it.next();
+			if (stack.getType() == Material.BLAZE_ROD || stack.getType() == Material.OBSIDIAN) {
+				e.getDrops().remove(stack);
+			}
+		}
+	}
 	
 	@EventHandler
 	public void onRespawn(PlayerRespawnEvent e) {
