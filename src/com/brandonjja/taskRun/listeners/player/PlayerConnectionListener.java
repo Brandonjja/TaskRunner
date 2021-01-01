52f58a5339669d1d89732f65b6d9da40053a6d90
Brandon <brandonjja@hotmail.com>
2021-01-01 06:18:12 -0400

    Prevent Player object being stored after disconnect

--- src/com/brandonjja/taskRun/listeners/player/PlayerConnectionListener.java
+++ src/com/brandonjja/taskRun/listeners/player/PlayerConnectionListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Achievement;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.brandonjja.taskRun.TaskRun;
 
@@ -13,5 +14,10 @@ public class PlayerConnectionListener implements Listener {
 	public void onJoin(PlayerJoinEvent e) {
 		TaskRun.addPlayer(e.getPlayer());
 		e.getPlayer().awardAchievement(Achievement.OPEN_INVENTORY);
+	}
+	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent e) {
+		TaskRun.removePlayer(e.getPlayer());
 	}
 }
